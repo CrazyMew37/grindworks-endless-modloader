97028d39fc148b10c180d67c0654573df1c23ceb
CrazyMew37 <64723948+CrazyMew37@users.noreply.github.com>
2025-06-02 20:37:24 -0500

    Delete CrazyMew37-EndlessMode/extensions/objects/items directory
    2.2 Update - Removed Bee Wings nerf

--- CrazyMew37-EndlessMode/extensions/objects/items/custom/item_bee_wings.hooks.gd
+++
@@ -1,7 +0,0 @@
-extends Object
-
-## Sync multipliers to current speed amount
-func on_speed_changed(chain: ModLoaderHookChain, speed: float) -> void:
-	chain.reference_object.multiplier.amount = maxf(0.0, (speed - 1.0) * 0.75)
-	if chain.reference_object.multiplier.amount > 0.75:
-		chain.reference_object.multiplier.amount = 0.75
